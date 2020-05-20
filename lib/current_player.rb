940dd2b5b0728f5c17a614d2226472916ee6c9c7
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-20 00:39:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,10 +4,17 @@ def turn_count(board)
   count = 0 
  
  board.each do |space|
+    
     if space == "X" || "O"
       count += 1 
+    else
+      count = count 
     end
+    
     count
+  
   end
+  
+  
   count
 end
