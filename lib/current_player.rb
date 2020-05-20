017e53f1c04445211895795433e2c771ec3f5be7
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-20 00:34:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-count = 0
+
 
 def turn_count(board)
-  board.each do |space|
+  count = 0 
+ 
+ board.each do |space|
     if space == "X" || "O"
       count += 1 
     else
